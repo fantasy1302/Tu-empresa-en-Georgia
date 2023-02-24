93ad4b60efd9fc3c684b5a2c3d82276679b6c820
Alexander Morozov <iceberginaction@gmail.com>
2023-02-24 19:39:08 +0400

    213

--- JS/testimonials-main.js
+++ JS/testimonials-main.js
@@ -1,18 +1,18 @@
-const testimonials = document.querySelectorAll('.testimonial');
-const dots = document.querySelectorAll('.dot');
-const bgImage = document.querySelector('.testimonials-section-bg');
-const bgColor = document.querySelector('.testimonials-section');
+const testimonials = document.querySelectorAll('.testimonials__block');
+const dots = document.querySelectorAll('.testimonials__button');
+const bgImage = document.querySelector('.testimonials__section--bg');
+const bgColor = document.querySelector('.testimonials__section');
 
 dots.forEach((dot, index) => {
   dot.addEventListener('click', () => {
     testimonials.forEach(testimonial => {
-      testimonial.classList.remove('ct-active');
+      testimonial.classList.remove('testimonials--active');
     });
-    testimonials[index].classList.add('ct-active');
+    testimonials[index].classList.add('testimonials--active');
     dots.forEach(dot => {
-      dot.classList.remove('dot-active');
+      dot.classList.remove('testimonials__button--active');
     });
-    dot.classList.add('dot-active');
+    dot.classList.add('testimonials__button--active');
     switch (index) {
       case 0:
         bgImage.style.transition = 'all 0.9s ease';
@@ -46,53 +46,4 @@ dots.forEach((dot, index) => {
         break;
     }
   });
-}); const testimonials = document.querySelectorAll('.testimonial');
-    const dots = document.querySelectorAll('.dot');
-    const bgImage = document.querySelector('.testimonials-section-bg');
-    const bgColor = document.querySelector('.testimonials-section');
-
-    dots.forEach((dot, index) => {
-      dot.addEventListener('click', () => {
-        testimonials.forEach(testimonial => {
-          testimonial.classList.remove('ct-active');
-        });
-        testimonials[index].classList.add('ct-active');
-        dots.forEach(dot => {
-          dot.classList.remove('dot-active');
-        });
-        dot.classList.add('dot-active');
-        switch (index) {
-          case 0:
-            bgImage.style.transition = 'all 0.9s ease';
-            bgImage.src = "Images/man-cafe.jpg";
-            bgColor.style.backgroundColor = '#2B6EEA';
-            bgColor.style.transition = 'all 0.9s ease';
-            break;
-          case 1:
-            bgImage.style.transition = 'all 0.9s ease';
-            bgImage.src = "Images/tech-bg.jpg";
-            bgColor.style.backgroundColor = '#00C4C4';
-            bgColor.style.transition = 'all 0.9s ease';
-            break;
-          case 2:
-            bgImage.style.transition = 'all 0.9s ease';
-            bgImage.src = "Images/bg-3.jpg";
-            bgColor.style.backgroundColor = '#ffcb57';
-            bgColor.style.transition = 'all 0.9s ease';
-            break;
-          case 3:
-            bgImage.style.transition = 'all 0.9s ease';
-            bgImage.src = "Images/bg-4.jpg";
-            bgColor.style.backgroundColor = '#ff333d';
-            bgColor.style.transition = 'all 0.9s ease';
-            break;
-          case 4:
-            bgImage.style.transition = 'all 0.9s ease';
-            bgImage.src = "Images/bg-5.jpg";
-            bgColor.style.backgroundColor = '#a960ee';
-            bgColor.style.transition = 'all 0.9s ease';
-            break;
-        }
-      });
-    });
-    });
+});
